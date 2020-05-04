32139351ed5e867c783750dd3c6ca2d187e009a6
Juan Pedro Guirado <43378763+jpguirado@users.noreply.github.com>
2020-05-04 19:47:47 +0200

    Creación proyecto Unity y versión básica escena Info PC
    Se ha creado el proyecto de Unity 2D para la aplicación. Se ha incluido una escena para el menú principal y otra para mostrar la info del equipo donde se ejecuta. Incluida navegación entre estas 2 escenas. Se muestra información básica sobre el hardware. Incluido también control para salir de la aplicación mediante un botón o presionando la tecla Esc.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    //Charge Info PC Scene
+    public void OnClickInfoPC()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("InfoPC");
+    }
+
+    //Quit application
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
